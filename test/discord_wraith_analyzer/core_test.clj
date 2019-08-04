b96a4a571d127fbc39fe2f8b58f0066539c4cf0b
Cheatinikle <kunwoo3526@gmail.com>
2019-08-04 19:33:52 +0900

    I was such a dumb... Passed emoji-handler test

--- test/discord_wraith_analyzer/core_test.clj
+++ test/discord_wraith_analyzer/core_test.clj
@@ -6,7 +6,7 @@
 (deftest emoji-test
   (testing "Can it detect emojis?"
     (reset! emoji-counts {})
-    (emoji-handler "!!" nil {:content ":unicorn: and :haskell:"})
-    (is (= (get ":unicorn:" @emoji-counts) 1))
-    (is (= (get ":haskell:" @emoji-counts) 1))))
-
+    (emoji-handler "!!" nil {:content "<:unicorn:125> and <:haskell:456>"})
+    (timbre/infof "Emoji : %s" @emoji-counts)
+    (is (= (get @emoji-counts "<:unicorn:125>") 1))
+    (is (= (get @emoji-counts "<:haskell:456>") 1))))
